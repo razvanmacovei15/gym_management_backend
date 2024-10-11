2f29c1f9efc8fe240141dfb85f29f52ec97ec915
razvanmc15 <razvanmc15@gmail.com>
2024-10-11 19:55:06 +0300

    develop push

--- Solution.java
+++ Solution.java
@@ -2,5 +2,6 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println("Hello WORLD!");
         System.out.println("Hello WORLD in development!");
+        System.out.println("Hello WORLD in this new feature!!");
     }
 }
