a3b15bc28e86a5ddfa0ce2d45b1410f111454f3e
razvanmc15 <razvanmc15@gmail.com>
2024-10-12 10:19:22 +0300

    try 04

--- Razvan.java
+++ Razvan.java
@@ -1,5 +1,6 @@
 public class Razvan {
     public static void main(String[] args) {
         System.out.println("Razvan did a thing on Github!");
+        System.out.println("this should work like this!");
     }
 }
--- Solution.java
+++ Solution.java
@@ -1,5 +1,7 @@
 public class Solution {
     public static void main(String[] args) {
         System.out.println("Hello WORLD!");
+
+        System.out.println("try number 4");
     }
 }
