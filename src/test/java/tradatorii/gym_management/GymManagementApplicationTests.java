a0324a70afd1abcdd0977b1dcf45fc9866a1900a
RazvanMacovei15 <razvanmc15@gmail.com>
2024-10-13 10:38:24 +0300

    first message

---
+++ src/test/java/tradatorii/gym_management/GymManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package tradatorii.gym_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
