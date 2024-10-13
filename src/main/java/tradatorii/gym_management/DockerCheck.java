32ee26323a17b5760453039717ed7cd320a9a660
RazvanMacovei15 <razvanmc15@gmail.com>
2024-10-14 01:53:14 +0300

    lets try the docker without the auto restart

--- src/main/java/tradatorii/gym_management/DockerCheck.java
+++ src/main/java/tradatorii/gym_management/DockerCheck.java
@@ -2,6 +2,6 @@ package tradatorii.gym_management;
 
 public class DockerCheck {
     public static void main(String[] args) {
-        System.out.println("Docker is running on another change another!");
+        System.out.println("Docker is running on another change another last time i swear!");
     }
 }
