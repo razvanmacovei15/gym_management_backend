d701aaad2bb477f3eabf4a8bd67edd5da92573fd
RazvanMacovei15 <razvanmc15@gmail.com>
2024-10-16 20:13:24 +0300

    live update!

--- src/main/java/tradatorii/gym_management/Controller/UserController.java
+++ src/main/java/tradatorii/gym_management/Controller/UserController.java
@@ -32,6 +32,6 @@ public class UserController {
 
     @GetMapping("/test")
     public ResponseEntity<String> testEndpoint() {
-        return ResponseEntity.ok("Ramo te rog ia-mi un PopCola sa am la tine acasa cand mai vin!\n If Alex is reading, whats up brother? \n Raul scuze ca am uitat de seminar:D lalala");
+        return ResponseEntity.ok("Ramo te rog ia-mi un PopCola sa am la tine acasa cand mai vin!\n If Alex is reading, whats up brother? \n Lets see if this updates the docker while it is still running!");
     }
 }
