de3d0a114f349d5ec76dcb6bec21a16bada59535
RazvanMacovei15 <razvanmc15@gmail.com>
2024-10-16 15:24:54 +0300

    repar

--- src/main/java/tradatorii/gym_management/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package tradatorii.gym_management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tradatorii.gym_management.entity.User;
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- src/main/java/tradatorii/gym_management/services/UserService.java
+++
@@ -1,7 +0,0 @@
-package tradatorii.gym_management.services;
-
-import tradatorii.gym_management.entity.User;
-
-public interface UserService {
-    void save(User user);
-}
