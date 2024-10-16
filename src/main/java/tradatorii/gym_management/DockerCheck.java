6c8951e23d5a8222a7733f5d1b90307372b593d6
RazvanMacovei15 <razvanmc15@gmail.com>
2024-10-16 19:46:04 +0300

    m

--- src/main/java/tradatorii/gym_management/DockerCheck.java
+++ src/main/java/tradatorii/gym_management/DockerCheck.java
@@ -3,8 +3,5 @@ package tradatorii.gym_management;
 public class DockerCheck {
     public static void main(String[] args) {
         System.out.println("Docker is running on another change another last time i swear!");
-        System.out.println("Lets check if the docker upload works now");
-        System.out.println("new check lalala-tralalala-razvan develop check");
-        System.out.println("new check lalala-tralalala-razvan develop check");
     }
 }
