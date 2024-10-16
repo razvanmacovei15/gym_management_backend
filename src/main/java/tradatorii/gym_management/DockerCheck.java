aab6f43f6f267dea3fc05a14406e66331465fbe6
Razvan Macovei <razvanmc15@gmail.com>
2024-10-16 10:08:00 +0300

    focking hell

--- src/main/java/tradatorii/gym_management/DockerCheck.java
+++ src/main/java/tradatorii/gym_management/DockerCheck.java
@@ -4,6 +4,6 @@ public class DockerCheck {
     public static void main(String[] args) {
         System.out.println("Docker is running on another change another last time i swear!");
         System.out.println("Lets check if the docker upload works now");
-        System.out.println("new check lalala");
+        System.out.println("new check lalala-tralalala");
     }
 }
