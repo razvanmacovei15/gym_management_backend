db07c8b20ac73330596fffc091c76bfd7d7a0424
RazvanMacovei15 <razvanmc15@gmail.com>
2024-11-02 13:00:15 +0200

    hai sa vedem de merge auto docker run

--- src/main/java/tradatorii/gym_management/DockerCheck.java
+++ src/main/java/tradatorii/gym_management/DockerCheck.java
@@ -3,5 +3,6 @@ package tradatorii.gym_management;
 public class DockerCheck {
     public static void main(String[] args) {
         System.out.println("lets update the code just a little bit more");
+        System.out.println("lets update the code just a little bit more: auto docker update!");
     }
 }
