6175c9535839fcabcc99aad9243e767a69fef8fa
Razvan Macovei <razvanmc15@gmail.com>
2024-12-18 12:27:05 +0200

    sa vdem de merge servicul de minio

--- src/main/java/tradatorii/gym_management/Controller/GymController.java
+++ src/main/java/tradatorii/gym_management/Controller/GymController.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tradatorii.gym_management.DTO.GymDTO;
+import tradatorii.gym_management.DTO.UserDTO;
 import tradatorii.gym_management.Entity.Gym;
 import tradatorii.gym_management.Entity.User;
 import tradatorii.gym_management.Enums.Role;
 import tradatorii.gym_management.Mappers.GymMapper;
+import tradatorii.gym_management.Mappers.UserMapper;
 import tradatorii.gym_management.Repo.GymRepo;
 import tradatorii.gym_management.Repo.UserRepo;
 import tradatorii.gym_management.Service.GymServiceInterface;
@@ -23,6 +25,7 @@ public class GymController {
     private final GymServiceInterface gymService;
 
     private final GymMapper gymMapper;
+    private final UserMapper userMapper;
     @Autowired
     private final GymRepo gymRepository;
     @Autowired
@@ -51,6 +54,13 @@ public class GymController {
         List<Gym> gyms = gymService.getAllGyms();
         List<GymDTO> gymDTOs = gyms.stream().map(gymMapper::toDTO).collect(Collectors.toList());
         return ResponseEntity.ok(gymDTOs);
+    }
+
+    @GetMapping("/getManagers")
+    public ResponseEntity<List<UserDTO>> getManagersByGymIds(@RequestParam List<Long> gymIds) {
+        List<User> managers = gymService.getManagersByGymIds(gymIds);
+        List<UserDTO> managersDTO = managers.stream().map(userMapper::toDTO).collect(Collectors.toList());
+        return ResponseEntity.ok(managersDTO);
     }
 
 }
