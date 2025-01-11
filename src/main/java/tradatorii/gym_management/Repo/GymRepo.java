961894a26134de7a68df870f8b49b741564766d4
razvanmc15 <razvanmc15@gmail.com>
2025-01-11 08:47:40 +0200

    am adaugat si restul de statuses la lista

--- src/main/java/tradatorii/gym_management/Repo/GymRepo.java
+++ src/main/java/tradatorii/gym_management/Repo/GymRepo.java
@@ -2,7 +2,9 @@ package tradatorii.gym_management.Repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import tradatorii.gym_management.DTO.GymBucket;
 import tradatorii.gym_management.Entity.Gym;
 import tradatorii.gym_management.Entity.User;
 
@@ -14,4 +16,8 @@ public interface GymRepo extends JpaRepository<Gym,Long> {
     List<User> findManagersByGymIds(List<Long> gymIds);
     Gym getGymByGymId(Long gymId);
     Gym findGymByManager(User manager);
+
+
+
+
 }
