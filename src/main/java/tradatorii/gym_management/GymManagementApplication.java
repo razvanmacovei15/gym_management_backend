0d7358012fe31139ff22fcfe392186b478927d45
razvanmacovei15 <razvanmc15@gmail.com>
2025-05-09 12:45:41 +0300

    initial commit: fresh laravel instalation

--- src/main/java/tradatorii/gym_management/GymManagementApplication.java
+++ src/main/java/tradatorii/gym_management/GymManagementApplication.java
@@ -13,3 +13,21 @@ public class GymManagementApplication {
     }
 
 }
+
+
+//hello there blue eyes,
+//
+//am reusit si ti-am scris mesajul/scrisorica(ca asta a devenit intre timp) de care ti-am zis acum ceva timp. a durat asa mult ca trebuit sa imi fac oridine bine in ganduri sa le pot pune pe "foaie" cat de bine pot eu
+//
+//la final o sa ai un link care te va duce pe platforma care am creat-o, un fel message exchange app, unde tu vei fi user 1, eu voi fi user 2
+//fiecare poate lasa celuilat un mesaj pe un fisier de tip markdown, si avem la dispozitie 10 minute pentru a citi
+//
+//ce ti-am scris iti ia aproximativ 3-4 minute de citit, si cred ca ai lejer timp sa treci de 2 ori prin ea
+//
+//te rog sa fii singura si fara agitatie in jurul tau cand o sa citesti ce e acolo(se poate lejer si de pe telefon daca bagi linkul in chrome sau ceva, gen poti si de la baie :)) ), vreau sa fii open minded si sa intelegi ca tot ce vreau eu este sa povestim pe ceva concret depinzand de ce parere o sa ai de ce am scris
+//
+//http://maco-coding.go.ro:5173
+//
+//happy reading
+//
+//o sa astept "cu sufletul la gura" un raspuns de la tine
